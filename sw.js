c837e0fc91e1c29c5acb239199d6f30ced42d183
Tristan Teufel <tristanteufel@googlemail.com>
2026-02-10 10:44:59 +0100

    feat: add icons for PWA and update manifest to use new icon files

--- sw.js
+++ sw.js
@@ -12,3 +12,7 @@ self.addEventListener('activate', (event) => {
         }).then(() => self.clients.claim())
     );
 });
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
