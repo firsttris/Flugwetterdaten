24e20bfd78d7bdf0ebedcc4c02aea4e29edc893c
Tristan Teufel <tristanteufel@googlemail.com>
2026-02-10 09:18:58 +0100

    chore: Remove redundant comments and blank lines from service worker.

--- sw.js
+++ sw.js
@@ -1,13 +1,8 @@
-// Minimaler Service Worker nur für PWA-Installation
-// KEIN Caching, KEIN Fetch-Handling - nur Installation ermöglichen
-
 self.addEventListener('install', (event) => {
-    // Sofort aktivieren, kein Warten
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    // Alle alten Caches löschen
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
@@ -16,5 +11,3 @@ self.addEventListener('activate', (event) => {
         }).then(() => self.clients.claim())
     );
 });
-
-// KEIN fetch-Event-Listener = Browser behandelt alle Requests normal
